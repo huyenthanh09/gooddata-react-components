bd6adfb8f7a3e9297dc4cb4ba39acda2f0f32a6b
TuBui <tu.bui@gooddata.com>
2020-06-18 11:42:38 +0700

    change_staging

--- examples/server/src/index.js
+++ examples/server/src/index.js
@@ -1,4 +1,4 @@
-// (C) 2007-2019 GoodData Corporation
+// (C) 2007-2020 GoodData Corporation
 /* eslint-disable no-console */
 const { factory } = require("@gooddata/gooddata-js");
 const https = require("https");
@@ -16,7 +16,7 @@ const config = {
     serveFrom: `${__dirname}/../../dist/`,
     port: process.env.PORT || 3009,
     https: process.env.HTTPS || false,
-    domain: process.env.DOMAIN || "https://developer.na.gooddata.com/",
+    domain: process.env.DOMAIN || "https://staging3.intgdc.com",
     domainAdmin: {
         username: process.env.DOMAIN_ADMIN_USERNAME,
         password: process.env.DOMAIN_ADMIN_PASSWORD,
