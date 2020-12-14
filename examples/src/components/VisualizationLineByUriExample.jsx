0549b09b6512602f139f3d57f4ebf5b87ee700e7
TuBui <tu.bui@gooddata.com>
2020-12-14 14:22:32 +0700

    update_1401_78

--- examples/src/components/VisualizationLineByUriExample.jsx
+++ examples/src/components/VisualizationLineByUriExample.jsx
@@ -1,15 +1,41 @@
-// (C) 2007-2018 GoodData Corporation
+// (C) 2007-2020 GoodData Corporation
 import React, { Component } from "react";
 import "@gooddata/react-components/styles/css/main.css";
 import { Visualization } from "@gooddata/react-components";
+import { HeaderPredicateFactory } from "@gooddata/react-components";
 
 import { projectId, lineVisualizationUri } from "../utils/fixtures";
 
 export class VisualizationTable extends Component {
     render() {
         return (
-            <div style={{ height: 300 }} className="s-visualization-line">
-                <Visualization projectId={projectId} uri={lineVisualizationUri} />
+            <div>
+                <p>hidden data points </p>
+                <div style={{ height: 300 }} className="s-visualization-line">
+                    <Visualization
+                        projectId={projectId}
+                        uri="/gdc/md/xp9yfghe4na21w27cyrnyrwx5si2vk6e/obj/12980"
+                        drillableItems={[
+                            HeaderPredicateFactory.uriMatch(
+                                "/gdc/md/xp9yfghe4na21w27cyrnyrwx5si2vk6e/obj/2364",
+                            ),
+                        ]}
+                    />
+                </div>
+                <p />
+                <div style={{ height: 300 }} className="s-visualization-line">
+                    <Visualization
+                        projectId={projectId}
+                        uri="/gdc/md/xp9yfghe4na21w27cyrnyrwx5si2vk6e/obj/12979"
+                    />
+                </div>
+                <p />
+                <div style={{ height: 300 }} className="s-visualization-line">
+                    <Visualization
+                        projectId={projectId}
+                        uri="/gdc/md/xp9yfghe4na21w27cyrnyrwx5si2vk6e/obj/12978"
+                    />
+                </div>
             </div>
         );
     }
